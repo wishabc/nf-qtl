3ef8c6721845a2a9c102c95216e70ee67df1ff29
wishabc <aswq22013@gmail.com>
2023-04-14 16:50:48 -0700

    debug

--- bin/qtl_regression.py
+++ bin/qtl_regression.py
@@ -237,7 +237,6 @@ def find_valid_samples(genotypes, cell_types, min_samples_per_genotype=3, unique
             )
         if valid_cell_types_mask.sum() == 0:
             continue
-        print(np.where(valid_cell_types_mask))
         res[snp_idx, :] = np.sum(cell_types[valid_cell_types_mask, :] != 0, axis=0) >= n_cell_types
 
     return res * (genotypes != -1).astype(bool) # [SNP x sample]
@@ -392,6 +391,7 @@ def main(chunk_id, masterlist_path, non_nan_mask_path, phenotype_matrix_path,
         valid_samples = find_valid_samples(bed, ohe_cell_types.T, 3, 3)  # [SNPs x samples]
         before_n = (bed != -1).sum()
         bed[~valid_samples] = -1
+        print(valid_samples.sum())
         testable_snps = find_testable_snps(bed, min_samples_per_genotype=3, unique_genotypes=3)
         bed = bed[testable_snps, :]  # [SNPs x indivs]
         snps_per_dhs = snps_per_dhs[:, testable_snps]  # [DHS x SNPs] boolean matrix
