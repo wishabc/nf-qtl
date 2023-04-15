d10b0f197ec43d6d81618535f628c2667f4f7912
wishabc <aswq22013@gmail.com>
2023-04-14 17:32:28 -0700

    debug

--- bin/qtl_regression.py
+++ bin/qtl_regression.py
@@ -388,7 +388,7 @@ def main(chunk_id, masterlist_path, non_nan_mask_path, phenotype_matrix_path,
         # Filter out cell-types with less than 2 distinct genotypes
         valid_samples = find_valid_samples(bed, ohe_cell_types.T,
             min_samples_per_genotype=3,
-            unique_genotypes=2,
+            unique_genotypes=3,
             n_cell_types=2)  # [SNPs x samples]
         before_n = (bed != -1).sum()
         bed[~valid_samples] = -1
