030ef636af8493d373dd0db6e78349e0ea1c333a
wishabc <aswq22013@gmail.com>
2023-04-14 17:08:25 -0700

    debug

--- bin/qtl_regression.py
+++ bin/qtl_regression.py
@@ -388,7 +388,7 @@ def main(chunk_id, masterlist_path, non_nan_mask_path, phenotype_matrix_path,
         ohe_enc = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
         ohe_cell_types = ohe_enc.fit_transform(cell_types.reshape(-1, 1))
         # Filter out cell-types with less than 2 distinct genotypes
-        valid_samples = find_valid_samples(bed, ohe_cell_types.T, 3, 3, n_cell_types=2)  # [SNPs x samples]
+        valid_samples = find_valid_samples(bed, ohe_cell_types.T, min_samples_per_genotype=3, unique_genotypes=3, n_cell_types=2)  # [SNPs x samples]
         before_n = (bed != -1).sum()
         bed[~valid_samples] = -1
         testable_snps = find_testable_snps(bed, min_samples_per_genotype=3, unique_genotypes=3)
